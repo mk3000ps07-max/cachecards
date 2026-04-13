c7990c9842eba9f6c8daac8107230df5a0ba508b
Manisha Kuamri P <mk3000ps07@gmail.com>
2026-04-13 20:55:12 +0530

    Serve frontend

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const app = require("./src/app");
 const config = require("./src/config/config");
 const connectDB = require("./src/db/db");
@@ -6,6 +7,7 @@ const PORT = config.PORT || 3000
 
 connectDB()
 
+app.use(express.static("frontend"));
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
